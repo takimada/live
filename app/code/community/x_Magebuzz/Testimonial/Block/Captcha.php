6857d91629b622f6567a2846f9e678d89a4face8
volkan <volkanaltan@gmail.com>
2014-09-24 22:20:55 +0300

    gokhangursen islemleri

--- app/code/community/x_Magebuzz/Testimonial/Block/Captcha.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Magebuzz_Testimonial_Block_Captcha extends Mage_Core_Block_Template {
-	public function __construct() {
-		parent::__construct();
-	}
-}
--- app/code/community/x_Magebuzz/Testimonial/Block/Form/Login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Magebuzz_Testimonial_Block_Form_Login extends Mage_Customer_Block_Form_Login {
-   
-}
